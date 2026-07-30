346b3180d01a35254431e3b9a78d96b91058c839
Priyanshu Dangare <priyanshu@ente.com>
2026-07-30 11:48:12 +0530

    [mobile][photos] refactor trash file object casting conditions

--- mobile/apps/photos/lib/ui/viewer/file/thumbnail_widget.dart
+++ mobile/apps/photos/lib/ui/viewer/file/thumbnail_widget.dart
@@ -18,7 +18,6 @@ import 'package:photos/models/api/collection/user.dart';
 import 'package:photos/models/file/extensions/file_props.dart';
 import 'package:photos/models/file/file.dart';
 import 'package:photos/models/file/file_type.dart';
-import 'package:photos/models/file/trash_file.dart';
 import 'package:photos/models/gallery_type.dart';
 import 'package:photos/module/download/file.dart';
 import 'package:photos/module/download/thumbnail.dart';
@@ -260,8 +259,8 @@ class _ThumbnailWidgetState extends State<ThumbnailWidget> {
       viewChildren.add(const UnSyncedIcon());
     }
 
-    if (widget.file.isTrash) {
-      viewChildren.add(TrashedFileOverlayText(widget.file as TrashFile));
+    if (widget.file.asTrashFile != null) {
+      viewChildren.add(TrashedFileOverlayText(widget.file.asTrashFile!));
     } else if (galleryContext?.type == GroupType.size) {
       viewChildren.add(FileSizeOverlayText(widget.file));
     } else if (widget.file.debugCaption != null) {
@@ -337,10 +336,10 @@ class _ThumbnailWidgetState extends State<ThumbnailWidget> {
           if (thumbData == null) {
             if (widget.file.isUploaded) {
               _logger.info("Removing localID reference for " + widget.file.tag);
-              if (widget.file is TrashFile) {
-                if (!(widget.file as TrashFile).isSystemOnly) {
+              if (widget.file.asTrashFile != null) {
+                if (!widget.file.asTrashFile!.isSystemOnly) {
                   widget.file.localID = null;
-                  unawaited(TrashDB.instance.update(widget.file as TrashFile));
+                  unawaited(TrashDB.instance.update(widget.file.asTrashFile!));
                 }
               } else {
                 widget.file.localID = null;
