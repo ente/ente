24ffd9d940f7b04abb6dcefcf250d3380f29461a
Priyanshu Dangare <priyanshu@ente.com>
2026-08-18 12:42:01 +0530

    [mobile][photos] fix covariance problems in file load result due to trash files

--- mobile/apps/photos/lib/models/file_load_result.dart
+++ mobile/apps/photos/lib/models/file_load_result.dart
@@ -1,8 +1,16 @@
-import 'package:photos/models/file/file.dart';
+import "package:photos/models/file/file.dart";
 
 class FileLoadResult {
-  final List<EnteFile> files;
-  final bool hasMore;
+  late final List<EnteFile> files;
+  late final bool hasMore;
 
-  FileLoadResult(this.files, this.hasMore);
+  FileLoadResult(List<EnteFile> files, this.hasMore) {
+    if (files.runtimeType == <EnteFile>[].runtimeType) {
+      // Runtime type is already List<EnteFile>, no need to create a copy.
+      this.files = files;
+    } else {
+      // If the runtime type of the list is not List<EnteFile>, then create a copy, to avoid covariance problems.
+      this.files = List<EnteFile>.of(files);
+    }
+  }
 }
