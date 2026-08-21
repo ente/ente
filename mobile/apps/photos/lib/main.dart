07851d31ca9dc392fbd890419d48ede4535a56a3
laurenspriem <laurens-pilot-git@lrns.me>
2026-08-21 15:32:13 +0530

    [mobile][photos] Skip the startup sync in an iOS main engine launched in the background

--- mobile/apps/photos/lib/main.dart
+++ mobile/apps/photos/lib/main.dart
@@ -710,20 +710,15 @@ Future<void> _homeWidgetSyncPeriodic() async {
 }
 
 Future<void> _scheduleFGSync(String caller) async {
-  await _sync(caller);
-  _scheduleNextFGSync();
-}
-
-void _scheduleNextFGSync() {
+  final bool isIOSWokenInBackground =
+      Platform.isIOS &&
+      WidgetsBinding.instance.lifecycleState != AppLifecycleState.resumed;
+  if (isIOSWokenInBackground) {
+    _logger.info("Skipping $caller sync, app is not resumed");
+  } else {
+    await _sync(caller);
+  }
   Future.delayed(kFGSyncFrequency, () async {
-    final bool isIOSWokenInBackground =
-        Platform.isIOS &&
-        WidgetsBinding.instance.lifecycleState != AppLifecycleState.resumed;
-    if (isIOSWokenInBackground) {
-      _logger.info("Skipping fgSyncCron, app is not resumed");
-      _scheduleNextFGSync();
-      return;
-    }
     unawaited(_scheduleFGSync('fgSyncCron'));
   });
 }
